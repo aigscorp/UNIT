3ffa0abc529b90d20d9b16a8677942eb4aadbddc
home web <andr_25@mail.ru>
2021-08-30 23:45:36 +0300

    chang masterpage

--- app/pages/masterpage.php
+++ app/pages/masterpage.php
@@ -28,6 +28,7 @@ class MasterPage extends \App\Pages\Base
     public $listWorkEmployees = [];
     public $list_model = [];
     public $list_size = [];
+    public $list_defect = [];
 //    public $select_work = "";
 
     public function __construct($params = null)
@@ -75,8 +76,6 @@ class MasterPage extends \App\Pages\Base
             }
         }
 
-
-
         $this->add(new Form('masterForm'));
         $this->masterForm->add(new DropDownChoice('workTypeMaster', $type_works))->onChange($this, 'workTypeMasterOnChange');
 
@@ -84,12 +83,19 @@ class MasterPage extends \App\Pages\Base
         $this->panelModelMaster->add(new DataView('listWorkMaster',
             new ArrayDataSource(new \Zippy\Binding\PropertyBinding($this, "list_model")),$this,'listWorkMasterOnRow'));
 
+        $this->add(new Label('modelNameSize'));
+
         $this->add(new Form('tableWorkForm'))->setVisible(false);
-        $this->tableWorkForm->add(new Label('modelNameSize'));
-        $this->tableWorkForm->add(new Label('showTypeWork'));
+        $this->tableWorkForm->add(new SubmitLink('editDefectModel'))->onClick($this, 'editDefectModelOnClick');
+//        $this->tableWorkForm->add(new Label('modelNameSize'));
+//        $this->tableWorkForm->add(new Label('showTypeWork'));
         $this->tableWorkForm->add(new DataView('listWorkModelMaster',
             new ArrayDataSource(new \Zippy\Binding\PropertyBinding($this, "list_size")), $this, 'listWorkModelMasterOnRow'));
 
+        $this->add(new Form('defectForm'))->setVisible(false);
+        $this->defectForm->add(new DataView('listDefectModel',
+            new ArrayDataSource(new \Zippy\Binding\PropertyBinding($this, "list_defect")), $this, 'listDefectModelOnRow'));
+        $this->defectForm->add(new TextArea('defectInfo'));
         $a = 1;
         $b = $a + 9;
     }
@@ -113,16 +119,20 @@ class MasterPage extends \App\Pages\Base
 //        var_dump($select);
         $this->panelModelMaster->listWorkMaster->Reload();
         $this->masterForm->workTypeMaster->setValue($val);
+
+        $this->tableWorkForm->setVisible(false);
+        $this->panelModelMaster->setVisible(true);
+        $this->defectForm->setVisible(false);
+        $this->modelNameSize->setVisible(false);
+
         $this->updateAjax(array('workTypeMaster'));
     }
 
     public function listWorkMasterOnRow($row)
     {
         $item = $row->getDataItem();
-
         $row->add(new ClickLink('modelName'))->onClick($this, 'modelNameOnClick');
         $row->modelName->setValue($item->model);
-
     }
 
     public function modelNameOnClick($sender)
@@ -152,9 +162,12 @@ class MasterPage extends \App\Pages\Base
             $this->list_size[] = new WorksSize($i+1, $size_master[0][$i], $size_master[1][$i], $size_master[2][$i]);
         }
         $this->tableWorkForm->listWorkModelMaster->Reload();
-        $this->tableWorkForm->modelNameSize->setText($item->model);
-        $this->tableWorkForm->showTypeWork->setText($work);
-        $this->masterForm->setVisible(false);
+        $this->modelNameSize->setText($item->model);
+        $this->modelNameSize->setVisible(true);
+//        $this->tableWorkForm->modelNameSize->setText($item->model);
+//        $this->tableWorkForm->showTypeWork->setText($work);
+//        $this->masterForm->setVisible(false);
+
         $this->panelModelMaster->setVisible(false);
         $this->tableWorkForm->setVisible(true);
     }
@@ -167,6 +180,43 @@ class MasterPage extends \App\Pages\Base
         $row->itemCountModel->setText($item->total_quantity - $item->master_quantity);
         $row->add(new Label('itemTotalModel', $item->total_quantity));
         $row->add(new Label('itemRemainModel'));
+    }
+
+    public function editDefectModelOnClick($sender)
+    {
+        $this->tableWorkForm->setVisible(false);
+        $this->defectForm->setVisible(true);
+
+        $this->list_defect = [];
+        $defects = ["Брак на коже", "Моя вина", "Клей", "Другое, вызвать Администратора"];
+//        $text = "";
+        for($i = 0; $i < count($this->list_size); $i++){
+            $id = $this->list_size[$i]->getID();
+            $sz = $this->list_size[$i]->size;
+            $defect = "";
+            if(count($defects) > $i) $defect = $defects[$i];
+            $this->list_defect[] = new WorksSize($id, $sz, "", "", $defect);
+//            $text .= $this->list_defect[$i]->__toString();
+        }
+//        var_dump($this->list_defect);
+//        $this->defectForm->defectInfo->setText($text);
+        $this->defectForm->listDefectModel->Reload();
+
+    }
+
+    public function listDefectModelOnRow($row)
+    {
+        $item = $row->getDataItem();
+        if($item->id == 1){
+//            $row->add(new Label('defectSize', $item->size));
+            $row->add(new DropDownChoice('defectSize', [40,41,42,43]/*$item->size*/));
+        }
+//        else{
+//            $row->defectSize->addOption($item->id, $item->size);
+//        }
+
+        $row->add(new Label('defectDescribe', $item->defect));
+        $row->add(new CheckBox('defectSelect'));
     }
 
 }
@@ -199,16 +249,21 @@ class WorksSize implements \Zippy\Interfaces\DataItem
     public $size;
     public $total_quantity;
     public $master_quantity;
-//    public $model;
+    public $defect;
 //    public $detail;
 
-    public function __construct($id, $size, $total_quantity, $master_quantity)
+    public function __construct($id, $size, $total_quantity, $master_quantity, $defect = "")
     {
         $this->id = $id;
         $this->size = $size;
         $this->total_quantity = $total_quantity;
         $this->master_quantity = $master_quantity;
-//        $this->detail = $detail;
+        $this->defect = $defect;
+    }
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return $this->id . ", " . $this->size . ", " . $this->defect;
     }
 
     public function getID() { return $this->id; }
